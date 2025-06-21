ebc83f8d17a4c7985d9a7d0d39f755e365ca9a80
Misha <darkrabbit240@gmail.com>
2025-06-21 20:14:42 +0300

    feat: enhance and optimize floor and ceil drawing

--- cub3D_bonus/src/raycast/draw.c
+++ cub3D_bonus/src/raycast/draw.c
@@ -6,110 +6,73 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 14:20:00 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/06/20 23:16:37 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/06/21 20:11:21 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	add_shadow(uint32_t *color, int *dist)
-{
-	double		inten;
-	uint8_t		*raw_pixel;
-
-	raw_pixel = (uint8_t *)color;
-	inten = (BLOCK_SIZE * MODIF_BRIGHT) / (double)*dist;
-	if (inten < 1)
-	{
-		*(++raw_pixel) *= inten;
-		*(++raw_pixel) *= inten;
-		*(++raw_pixel) *= inten;
-	}
-}
-
-void	map_wall(t_raycast *raycast, int y, int wall_h, int wall_top)
+void	draw_wall(t_raycast *raycast, int *ptr_y, t_wall *wall)
 {
 	int			tex_y;
 	uint32_t	color;
-	uint8_t		*raw_pixel;
 	int			pixel_i;
+	int			cur_y;
 
-	tex_y = (y - wall_top) * BLOCK_SIZE / wall_h;
-	pixel_i = (tex_y * raycast->wall_img->width + raycast->tex_x) * BPP;
-	raw_pixel = &raycast->wall_img->pixels[pixel_i];
-	color = extract_rgba(raw_pixel);
-	add_shadow(&color, &raycast->data->rays_dist[raycast->cur_ray]);
-	mlx_put_pixel(raycast->scr_img, raycast->cur_ray, y, color);
+	cur_y = *ptr_y;
+	while (cur_y < wall->top + wall->h && cur_y < (int)raycast->scr_img->height)
+	{
+		tex_y = (cur_y - wall->top) * BLOCK_SIZE / wall->h;
+		pixel_i = (tex_y * wall->img->width + wall->tex_pos.x) * BPP;
+		color = extract_rgba(&wall->img->pixels[pixel_i]);
+		add_shadow(&color, raycast->data->rays_dist[raycast->cur_ray]);
+		mlx_put_pixel(raycast->scr_img, raycast->cur_ray, cur_y, color);
+		cur_y++;
+	}
+	*ptr_y = cur_y;
 }
-void	draw_floor(t_raycast *raycast, int y)
+
+void	draw_sky(t_raycast *raycast, int y, int wall_top)
 {
-	int		dist;
-	double		ratio;
-	t_point		floor_tex;
-	mlx_image_t	*img;
-	int			texX;
-	int			texY;
+	mlx_image_t	*sky;
+	t_point		tex;
 	int			pixel_i;
 	uint32_t	color;
-	uint8_t		*raw_pixel;
 
-	img = raycast->data->mlx_data.textrs_img[GROUND_TEX];
-
-	//double st = (double)BLOCK_SIZE / 2;
-	double st = BLOCK_SIZE / (1 + ((BLOCK_SIZE / 2) / raycast->data->player.height));
-
-	double b_cos = cos(deg_rad(raycast->beta));
-	double a_cos = cos(deg_rad(raycast->ray_angle));
-	double a_sin = sin(deg_rad(raycast->ray_angle));
-
-	while (y < (int)raycast->scr_img->height)
+	sky = raycast->data->mlx_data.textrs_img[SKY_TEX];
+	tex.x = (raycast->ray_angle / 360.0) * sky->width;
+	while (y < wall_top && y < (int)raycast->scr_img->height)
 	{
-		ratio = st / (y - raycast->plane->center.y);
-		dist = raycast->plane->dist * ratio;
-		dist /= b_cos;
-
-		floor_tex.x = dist * a_cos;
-		floor_tex.y = dist * -a_sin;
-
-		floor_tex.x += raycast->char_pos.x;
-		floor_tex.y += raycast->char_pos.y;
-
-
-		if (raycast->unit_map[floor_tex.y][floor_tex.x] == FLOOR)
-		{
-			img = raycast->data->mlx_data.textrs_img[FLOOR_TEX];
-		}
-		else
-		{
-			img = raycast->data->mlx_data.textrs_img[GROUND_TEX];
-		}
-
-		texX = floor_tex.x % BLOCK_SIZE;
-		texY = abs(floor_tex.y % BLOCK_SIZE);
-
-		pixel_i = (texY * img->width + texX) * BPP;
-		raw_pixel = &img->pixels[pixel_i];
-		color = extract_rgba(raw_pixel);
-		add_shadow(&color, &dist);
+		tex.y = sky->height - (raycast->plane->center.y - y);
+		pixel_i = (tex.y * sky->width + tex.x) * BPP;
+		color = extract_rgba(&sky->pixels[pixel_i]);
 		mlx_put_pixel(raycast->scr_img, raycast->cur_ray, y, color);
 		y++;
 	}
 }
 
-void	draw_sky(t_raycast *raycast, int y, int wall_top)
+void	draw_floor(t_raycast *raycast, int y)
 {
-	mlx_image_t *sky = raycast->data->mlx_data.textrs_img[SKY_TEX];
-	int tex_x = (raycast->ray_angle / 360.0) * sky->width;
+	t_point		floor_pos;
+	mlx_image_t	*img;
+	int			pixel_i;
+	uint32_t	color;
+	int			dist;
 
-	while (y < wall_top && y < (int)raycast->scr_img->height)
+	dist = 0;
+	while (y < (int)raycast->scr_img->height)
 	{
-		int	tex_y = sky->height - (raycast->plane->center.y - y);
-		if (tex_y < 0) tex_y = 0;
-		if (tex_y >= (int)sky->height) tex_y = sky->height - 1;
-
-		int pixel_i = (tex_y * sky->width + tex_x) * BPP;
-		uint8_t *raw_pixel = &sky->pixels[pixel_i];
-		uint32_t color = extract_rgba(raw_pixel);
+		fill_floor_info(raycast, &floor_pos, &dist, y);
+		if (raycast->unit_map[floor_pos.y][floor_pos.x] == FLOOR
+			|| raycast->unit_map[floor_pos.y][floor_pos.x] == DOOR)
+			img = raycast->data->mlx_data.textrs_img[FLOOR_TEX];
+		else
+			img = raycast->data->mlx_data.textrs_img[GROUND_TEX];
+		floor_pos.x = floor_pos.x % BLOCK_SIZE;
+		floor_pos.y = abs(floor_pos.y % BLOCK_SIZE);
+		pixel_i = (floor_pos.y * img->width + floor_pos.x) * BPP;
+		color = extract_rgba(&img->pixels[pixel_i]);
+		add_shadow(&color, dist);
 		mlx_put_pixel(raycast->scr_img, raycast->cur_ray, y, color);
 		y++;
 	}
@@ -117,109 +80,40 @@ void	draw_sky(t_raycast *raycast, int y, int wall_top)
 
 void	draw_ceiling(t_raycast *raycast, int wall_top, int y, int *p_y)
 {
-	int		dist;
-	double		ratio;
-	t_point		ceil_tex;
+	t_point		ceil_pos;
 	mlx_image_t	*img;
-	int			texX;
-	int			texY;
 	int			pixel_i;
 	uint32_t	color;
-	uint8_t		*raw_pixel;
+	int			dist;
 
 	img = raycast->data->mlx_data.textrs_img[CEIL_TEX];
-
-	//double st = (double)BLOCK_SIZE / 2;
-	double st = BLOCK_SIZE / (1 + ((BLOCK_SIZE / 2) / raycast->data->player.height));
-
-	double b_cos = cos(deg_rad(raycast->beta));
-	double a_cos = cos(deg_rad(raycast->ray_angle));
-	double a_sin = sin(deg_rad(raycast->ray_angle));
-
+	dist = 0;
 	while (y < (int)raycast->scr_img->height && y < wall_top)
 	{
-		ratio = st / (raycast->plane->center.y - y);
-		dist = raycast->plane->dist * ratio;
-		dist /= b_cos;
-
-		ceil_tex.x = dist * a_cos;
-		ceil_tex.y = dist * -a_sin;
-
-		ceil_tex.x += raycast->char_pos.x;
-		ceil_tex.y += raycast->char_pos.y;
-
-		if (is_on_map(raycast->data, &ceil_tex)
-			&& (raycast->unit_map[ceil_tex.y][ceil_tex.x] == FLOOR
-			|| raycast->unit_map[ceil_tex.y][ceil_tex.x] == DOOR))
+		fill_ceil_info(raycast, &ceil_pos, &dist, y);
+		if ((raycast->unit_map[ceil_pos.y][ceil_pos.x] == FLOOR
+			|| raycast->unit_map[ceil_pos.y][ceil_pos.x] == DOOR))
 		{
-			texX = ceil_tex.x % BLOCK_SIZE;
-			texY = abs(ceil_tex.y % BLOCK_SIZE);
-
-			pixel_i = (texY * img->width + texX) * BPP;
-			raw_pixel = &img->pixels[pixel_i];
-			color = extract_rgba(raw_pixel);
-			add_shadow(&color, &dist);
+			ceil_pos.x = ceil_pos.x % BLOCK_SIZE;
+			ceil_pos.y = abs(ceil_pos.y % BLOCK_SIZE);
+			pixel_i = (ceil_pos.y * img->width + ceil_pos.x) * BPP;
+			color = extract_rgba(&img->pixels[pixel_i]);
+			add_shadow(&color, dist);
 			mlx_put_pixel(raycast->scr_img, raycast->cur_ray, y, color);
 		}
 		y++;
 	}
-	*p_y = y - 1;
+	*p_y = y;
 }
 
-void	render_col(t_raycast *raycast, t_wall *wall, int wall_dist,
-		int tex_indx)
+void	render_col(t_raycast *raycast, t_wall *wall)
 {
-	int	wall_h;
-	int	wall_top;
 	int	y;
 
-	wall_h = ceil((BLOCK_SIZE) * raycast->plane->dist / (double)wall_dist);
-	wall_top = raycast->plane->center.y;
-
-	double h_ratio = 1 + (raycast->data->player.height / (BLOCK_SIZE / 2));
-
-	wall_top = wall_top - (wall_h / h_ratio);
-
-	if (raycast->axis == HORIZONT)
-	{
-		raycast->tex_x = wall->pos.x % BLOCK_SIZE;
-		if (wall->type == DOOR)
-		{
-			raycast->tex_x = BLOCK_SIZE - (wall->door_len - raycast->tex_x);
-		}
-	}
-	else if (raycast->axis == VERTICAL)
-	{
-		raycast->tex_x = wall->pos.y % BLOCK_SIZE;
-		if (wall->type == DOOR)
-		{
-			raycast->tex_x = BLOCK_SIZE - (wall->door_len - raycast->tex_x);
-		}
-	}
-
-	raycast->wall_img = raycast->data->mlx_data.textrs_img[tex_indx];
-
 	y = 0;
-
-	draw_sky(raycast, y, wall_top);
-
-	while (y < (int)raycast->scr_img->height)
-	{
-		if (y < wall_top)
-		{
-			// mlx_put_pixel(raycast->scr_img, raycast->cur_ray, y,
-			// 	raycast->ceil_rgbt);
-			//draw_ceiling(raycast, wall_top, y, &y);
-		}
-		else if (y < wall_top + wall_h)
-			map_wall(raycast, y, wall_h, wall_top);
-		else
-		{
-			break ;
-			// mlx_put_pixel(raycast->scr_img, raycast->cur_ray, y,
-			// 	raycast->flor_rgbt);
-		}
-		y++;
-	}
+	fill_wall_info(raycast, wall);
+	draw_sky(raycast, y, wall->top);
+	draw_ceiling(raycast, wall->top, y, &y);
+	draw_wall(raycast, &y, wall);
 	draw_floor(raycast, y);
 }
---
+++ cub3D_bonus/src/raycast/draw_utils.c
@@ -0,0 +1,63 @@
+#include "cub3D.h"
+
+void	add_shadow(uint32_t *color, int dist)
+{
+	double	alpha_mod;
+	uint8_t	*raw_pixel;
+
+	raw_pixel = (uint8_t *)color;
+	alpha_mod = (BLOCK_SIZE * MODIF_BRIGHT) / (double)dist;
+	if (alpha_mod < 1)
+	{
+		*(++raw_pixel) *= alpha_mod;
+		*(++raw_pixel) *= alpha_mod;
+		*(++raw_pixel) *= alpha_mod;
+	}
+}
+
+void	fill_wall_info(t_raycast *raycast, t_wall *wall)
+{
+	double	h_ratio;
+
+	wall->h = ceil(BLOCK_SIZE * raycast->plane->dist / (double)wall->dist);
+	wall->top = raycast->plane->center.y;
+	h_ratio = 1 + (raycast->data->player.height / (BLOCK_SIZE / 2));
+	wall->top = wall->top - (wall->h / h_ratio);
+	if (raycast->axis == HORIZONT)
+	{
+		wall->tex_pos.x = wall->pos.x % BLOCK_SIZE;
+		if (wall->type == DOOR)
+			wall->tex_pos.x = BLOCK_SIZE - (wall->door_len - wall->tex_pos.x);
+	}
+	else if (raycast->axis == VERTICAL)
+	{
+		wall->tex_pos.x = wall->pos.y % BLOCK_SIZE;
+		if (wall->type == DOOR)
+			wall->tex_pos.x = BLOCK_SIZE - (wall->door_len - wall->tex_pos.x);
+	}
+	wall->img = raycast->data->mlx_data.textrs_img[wall->img_i];
+}
+
+void	fill_floor_info(t_raycast *raycast, t_point *ceil_pos, int *dist, int y)
+{
+	double		st;
+	double		ratio;
+
+	st = BLOCK_SIZE / (1 + ((BLOCK_SIZE / 2) / raycast->data->player.height));
+	ratio = st / (y - raycast->plane->center.y);
+	*dist = (raycast->plane->dist * ratio) / raycast->angl_table->beta;
+	ceil_pos->x = (*dist * raycast->angl_table->cos) + raycast->char_pos.x;
+	ceil_pos->y = (*dist * -raycast->angl_table->sin) + raycast->char_pos.y;
+}
+
+void	fill_ceil_info(t_raycast *raycast, t_point *ceil_pos, int *dist, int y)
+{
+	double		st;
+	double		ratio;
+
+	st = BLOCK_SIZE / (1 + ((BLOCK_SIZE / 2) / raycast->data->player.height));
+	ratio = st / (raycast->plane->center.y - y);
+	*dist = (raycast->plane->dist * ratio) / raycast->angl_table->beta;
+	ceil_pos->x = (*dist * raycast->angl_table->cos) + raycast->char_pos.x;
+	ceil_pos->y = (*dist * -raycast->angl_table->sin) + raycast->char_pos.y;
+}
